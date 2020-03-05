f39d2b20cfef3dc4d208a0324be9391f3f092230
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 10:01:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+ 
+dwarves.each_with_index { |item, index | puts "#{index+1} #{item}" }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  
+  veggies.map { | v | v.capitalize + "!" }
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  
+  calls_long.any? { | w | w.length > 4  }
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(ziis)
+ cheese_types = ["cheddar", "gouda", "camembert"]
+ 
+ counter = 0 
+ 
+ while counter < ziis.length do 
+   if  ziis.include?(cheese_types[counter])
+     return cheese_types[counter]
+   end 
+   counter += 1 
+ 
+ 
+ 
+ end 
 end
